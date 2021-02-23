f3307b4c4dcb74f6b1080b7f56dd6e67a993a46c
Juan Manuel Duarte <69604488+puma001@users.noreply.github.com>
2021-02-23 15:59:03 -0500

    Entrega1

---
+++ TallerNewton/aitken.R
@@ -0,0 +1,24 @@
+aitken <- function(f, x0, nmax = 12, tol = 1e-8, ...) {
+  if (!is.numeric(x0) || length(x0) != 1)
+    stop("Argument 'x0' must be a numeric scalar.")
+  fun <- match.fun(f)
+  f <- function(x) fun(x, ...)
+  x <- x0
+  diff <- 1 + tol
+  niter <- 0
+  while (diff > tol && niter <= nmax) {
+    gx <- f(x)
+    ggx <- f(gx)
+    xnew <- (x*ggx - gx^2) / (ggx - 2*gx + x)
+    diff <- abs(x - xnew)
+    x <- xnew
+    niter <- niter + 1
+  }
+  if (niter > nmax)
+    warning("Maximum number of iterations exceeded.")
+  return(x)
+}
+# Find a zero of    f(x) = cos(x) - x*exp(x)
+# as fixpoint of  phi(x) = x + (cos(x) - x*exp(x))/2
+phi <- function(x) x + (cos(x) - x*exp(x))/2
+aitken(phi, 0)  #=> 0.5177574
